9d9aa4607cf5a1228f9a12900f616ff0d9515372
Javier <javiergarcferrer@gmail.com>
2023-07-31 14:46:26 -0400

    refactor with sites_config.json file

--- main_product_scraper.py
+++ main_product_scraper.py
@@ -1,60 +1,61 @@
 import inquirer
 import product_scraper
+import json
+import logging
 
-# Define the scraper configurations for each website
-sites = {
-    'LifestyleGarden': {
-        'url': 'https://lifestylegarden.com/all-outdoor',
-        'link_tag': '.woocommerce-LoopProduct-link',
-        'fields': {
-            'title': '.et_pb_wc_title',
-            'brief': '.et_pb_wc_description',
-            'description': '.et-dynamic-content-woo--product_description',
-            'category': '.et_pb_accordion_item_5_tb_body h5'
-        },
-        'image_tag': '.et_pb_gallery_image a',
-        'filename': 'muebles.json',
-        'pagination': 'c-pagination'
-    },
-    'El Catador': {
-        'url': 'https://tiendaenlinea.elcatador.com/collections/all',
-        'link_tag': '.c-product-card__top',
-        'fields': {
-            'name': '.c-product__title',
-            'hook': '.c-product-description__title',
-            'description': '.c-product-description__info span:last-child',
-            'price': '.c-price'
-        },
-        'image_tag': '.c-product-slide img',
-        'filename': 'vinos.json',
-        'pagination': '.c-pagination'
-    }
-}
+logging.basicConfig(level=logging.INFO)
 
-def main():
-    # Ask the user to select a site from the list
+
+def load_sites(filename):
+    """Load site data from given JSON file."""
+    try:
+        with open(filename, 'r') as f:
+            sites = json.load(f)
+        return sites
+    except FileNotFoundError:
+        logging.error(f'File {filename} not found.')
+    except json.JSONDecodeError:
+        logging.error(f'Error decoding JSON from {filename}.')
+    except Exception as e:
+        logging.error(f'Unexpected error: {e}')
+
+
+def get_user_input(sites):
+    """Prompts user to select a site for scraping."""
     questions = [
         inquirer.List('site',
                       message="Which site do you want to scrape?",
                       choices=sites.keys(),
-                  ),
+                      ),
     ]
-    answers = inquirer.prompt(questions)
-    site = answers['site']
+    return inquirer.prompt(questions)
 
-    # Get the configuration for the selected website
-    site_config = sites[site]
 
-    # Call the scraper with the selected site's configuration
-    product_scraper.scrape_site(
-        site_config['url'], 
-        site_config['link_tag'], 
-        site_config['fields'], 
-        site_config['filename'], 
-        site_config['image_tag'], 
-        site_config['pagination']
-    )
+def scrape_selected_site(site_config):
+    """Calls the scraper with the selected site's configuration."""
+    try:
+        product_scraper.scrape_site(
+            site_config['url'], 
+            site_config['link_tag'], 
+            site_config['fields'], 
+            site_config['filename'], 
+            site_config['image_tag'], 
+            site_config['pagination']
+        )
+    except Exception as e:
+        logging.error(f"Error occurred while scraping site: {e}")
+
+
+def main():
+    """Main function to load site data and initiate the scraping process."""
+    sites = load_sites("sites_config.json")
+
+    if sites:
+        answers = get_user_input(sites)
+        site = answers['site']
+
+        scrape_selected_site(sites[site])
+
 
 if __name__ == "__main__":
     main()
-
