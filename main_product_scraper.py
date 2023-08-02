737779f0a10f56e26996581611d769c0ae69c942
Javier <javiergarcferrer@gmail.com>
2023-08-01 22:07:54 -0400

    refactor as ProductScraper class

--- main_product_scraper.py
+++ main_product_scraper.py
@@ -1,5 +1,5 @@
 import inquirer
-import product_scraper
+from product_scraper import ProductScraper
 import json
 import logging
 
@@ -7,7 +7,6 @@ logging.basicConfig(level=logging.INFO)
 
 
 def load_sites(filename):
-    """Load site data from given JSON file."""
     try:
         with open(filename, 'r') as f:
             sites = json.load(f)
@@ -21,7 +20,6 @@ def load_sites(filename):
 
 
 def get_user_input(sites):
-    """Prompts user to select a site for scraping."""
     questions = [
         inquirer.List('site',
                       message="Which site do you want to scrape?",
@@ -31,30 +29,23 @@ def get_user_input(sites):
     return inquirer.prompt(questions)
 
 
-def scrape_selected_site(site_config):
-    """Calls the scraper with the selected site's configuration."""
-    try:
-        product_scraper.scrape_site(
-            site_config['url'], 
-            site_config['link_tag'], 
-            site_config['fields'], 
-            site_config['filename'], 
-            site_config['image_tag'], 
-            site_config['pagination']
-        )
-    except Exception as e:
-        logging.error(f"Error occurred while scraping site: {e}")
-
-
 def main():
-    """Main function to load site data and initiate the scraping process."""
     sites = load_sites("sites_config.json")
 
     if sites:
         answers = get_user_input(sites)
         site = answers['site']
+        site_config = sites[site]
 
-        scrape_selected_site(sites[site])
+        scraper = ProductScraper(
+            url=site_config['url'], 
+            link_tag=site_config['link_tag'], 
+            fields=site_config['fields'], 
+            filename=site_config['filename'], 
+            image_tag=site_config['image_tag'], 
+            pagination=site_config['pagination']
+        )
+        scraper.scrape_site()
 
 
 if __name__ == "__main__":
--- product_scraper.py
+++ product_scraper.py
@@ -7,78 +7,89 @@ import re
 
 logging.basicConfig(level=logging.INFO)
 
-def get_next_page_link(request, base_url, pagination):
-    """Get link of the next page."""
-    if pagination:
-        pagination_element = request.html.find(pagination, first=True)
-        if pagination_element:
-            next_page_link = pagination_element.find('a')[-1].attrs['href']
-            next_page_link = f"{base_url}/{next_page_link}" if not next_page_link.startswith('http') else next_page_link
-            if next_page_link == base_url:
-                next_page_link = None
-            return next_page_link
-    return None
+class ProductScraper:
+    
+    def __init__(self, url, link_tag, fields, filename, image_tag=None, pagination=None):
+        self.url = url
+        self.link_tag = link_tag
+        self.fields = fields
+        self.filename = filename
+        self.image_tag = image_tag
+        self.pagination = pagination
+        self.all_products_data = []
+        self.session = HTMLSession()
 
-def get_product_links(url: str, link_tag: str, pagination=None) -> [str]:
-    """Get product links on a page."""
-    session = HTMLSession()
-    request = session.get(url)
-    products = request.html.find(link_tag)
-    base_url = re.search(r'(https?://[^/]+)/', url).group(1)
-    product_links = [product.find('a', first=True).attrs['href'] for product in products]
-    product_links = [link if link.startswith('http') else base_url + link for link in product_links]
-    next_page_link = get_next_page_link(request, base_url, pagination)
+    def get_next_page_link(self, request, base_url):
+        """Get link of the next page."""
+        if self.pagination:
+            pagination_element = request.html.find(self.pagination, first=True)
+            if pagination_element:
+                next_page_link = pagination_element.find('a')[-1].attrs['href']
+                next_page_link = f"{base_url}/{next_page_link}" if not next_page_link.startswith('http') else next_page_link
+                if next_page_link == base_url:
+                    next_page_link = None
+                return next_page_link
+        return None
 
-    return product_links, next_page_link
+    def get_product_links(self, url, link_tag, pagination=None) -> [str]:
+        """Get product links on a page."""
+        request = self.session.get(url)
+        products = request.html.find(link_tag)
+        base_url = re.search(r'(https?://[^/]+)/', url).group(1)
+        product_links = [product.find('a', first=True).attrs['href'] for product in products]
+        product_links = [link if link.startswith('http') else base_url + link for link in product_links]
+        next_page_link = self.get_next_page_link(request, base_url)
 
-def extract_images(request, image_tag):
-    """Extract images from the product page."""
-    image_elements = request.html.find(image_tag)
-    image_sources = []
-    for image in image_elements:
-        try:
-            image_sources.append({'src': image.attrs['data-src']})
-        except KeyError:
+        return product_links, next_page_link
+
+    def extract_images(self, request):
+        """Extract images from the product page."""
+        image_elements = request.html.find(self.image_tag)
+        image_sources = []
+        for image in image_elements:
             try:
-                image_sources.append({'src': image.attrs['href']})
+                image_sources.append({'src': image.attrs['data-src']})
             except KeyError:
-                break
-    return image_sources
-
-def scrape_product_info(product_link: str, fields: dict, image_tag=None) -> dict:
-    """Scrape product information."""
-    session = HTMLSession()
-    request = session.get(product_link)
-    product_data = {}
-    for key, selector in fields.items():
-        el = request.html.find(selector, first=True)
-        product_data[key] = el.full_text.strip() if el else ''
-    product_data['url'] = product_link
-
-    if image_tag:
-        product_data['images'] = extract_images(request, image_tag)
-
-    return {'product': product_data}
-
-def scrape_site(url: str, link_tag: str, fields: dict, filename: str, image_tag=None, pagination=None):
-    """Scrape site for products information."""
-    all_products_data = []
-    page_number = 1
-
-    while url:
-        product_links, url = get_product_links(url, link_tag, pagination)
-
-        with progressbar.ProgressBar(max_value=len(product_links), 
-                                     prefix=f'Page {page_number}: ') as bar:
-            for i, product_link in enumerate(product_links):
                 try:
-                    product_data = scrape_product_info(product_link, fields, image_tag)
-                    all_products_data.append(product_data)
-                except Exception as e:
-                    logging.error(f'Error occurred while scraping {product_link}: {e}')
-                bar.update(i)
+                    image_sources.append({'src': image.attrs['href']})
+                except KeyError:
+                    break
+        return image_sources
 
-        page_number += 1
+    def scrape_product_info(self, product_link) -> dict:
+        """Scrape product information."""
+        session = self.session
+        request = session.get(product_link)
+        product_data = {}
+        for key, selector in self.fields.items():
+            el = request.html.find(selector, first=True)
+            product_data[key] = el.full_text.strip() if el else ''
+        product_data['url'] = product_link
 
-    with open(filename, 'w') as f:
-        json.dump(all_products_data, f, indent=4)
+        if self.image_tag:
+            product_data['images'] = self.extract_images(request)
+
+        return {'product': product_data}
+
+    def scrape_site(self):
+        """Scrape site for products information."""
+        url = self.url
+        page_number = 1
+
+        while url:
+            product_links, url = self.get_product_links(self.url, self.link_tag, self.pagination)
+
+            with progressbar.ProgressBar(max_value=len(product_links), 
+                                         prefix=f'Page {page_number}: ') as bar:
+                for i, product_link in enumerate(product_links):
+                    try:
+                        product_data = self.scrape_product_info(product_link)
+                        self.all_products_data.append(product_data)
+                    except Exception as e:
+                        logging.error(f'Error occurred while scraping {product_link}: {e}')
+                    bar.update(i)
+
+            page_number += 1
+
+        with open(self.filename, 'w') as f:
+            json.dump(self.all_products_data, f, indent=4)
